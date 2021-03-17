5c4a221a705b35fee466c7fd18eecf43d4fa6cc4
Esteban <estabanar@unicauca.edu.co>
2021-03-17 02:35:56 -0500

    JavaScript activate

--- css/main.js
+++ css/main.js
@@ -7,4 +7,12 @@ ScrollReveal().reveal('.banner');
 ScrollReveal().reveal('.presentation', {delay: 500});
 ScrollReveal().reveal('.xp-first', {delay: 500});
 ScrollReveal().reveal('.xp-last', {delay: 500});
+ScrollReveal().reveal('.university', {delay: 500});
+ScrollReveal().reveal('.technical', {delay: 500});
+ScrollReveal().reveal('.languages', {delay: 500});
+ScrollReveal().reveal('.computer-lan', {delay: 500});
+ScrollReveal().reveal('.dktp-tools', {delay: 500});
+ScrollReveal().reveal('.voluntary-xp', {delay: 500});
+ScrollReveal().reveal('#contact', {delay: 500});
 ScrollReveal().reveal('.work-in-p', {delay: 500});
+
